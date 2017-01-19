227b61a4a579e47134516d3654823c9749cecab8
Marcos Junior <junalmeida@gmail.com>
2017-01-19 11:42:18 -0200

    Fix deploy ci

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -6,5 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Alma Sistemas 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
 [assembly: ComVisible(false)]
