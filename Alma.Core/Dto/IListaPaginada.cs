77a8d842761a887d2b9f7fc0a08fd87ec32b401a
Marcos Junior <junalmeida@gmail.com>
2017-01-20 10:15:22 -0200

    Acertando locais de lista.

---
+++ Alma.Core/Dto/IListaPaginada.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Alma.Core.Dto
+{
+    public interface IListaPaginada<T> where T : class
+    {
+        IList<T> Lista { get; }
+        int Pagina { get; }
+        int TamanhoPagina { get; }
+        int TotalItens { get; }
+    }
+}
---
+++ Alma.Dominio/ListaPaginada.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Alma.Core.Dto
+{
+    public class ListaPaginada<T> where T : class
+    {
+        public const int TamanhoPaginaPadrao = 10;
+
+        public ListaPaginada(int pagina, int totalItens, IList<T> lista, int tamanhoPagina = TamanhoPaginaPadrao)
+        {
+            this.Pagina = pagina;
+            this.TamanhoPagina = tamanhoPagina;
+            this.TotalItens = totalItens;
+            this.Lista = lista;
+        }
+
+        public virtual int Pagina { get; private set; }
+        public virtual int TamanhoPagina { get; private set; }
+        public virtual int TotalItens { get; private set; }
+        public virtual IList<T> Lista { get; private set; }
+    }
+}
