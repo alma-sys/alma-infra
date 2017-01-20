7869048e4fb0008a4bb6505ba1f64e1594b7ff62
Marcos Junior <junalmeida@gmail.com>
2017-01-20 13:02:41 -0200

    Inclusão do IIdNome

---
+++ Alma.Core/Dto/IdNome.cs
@@ -0,0 +1,28 @@
+﻿namespace Alma.Core.Dto
+{
+    public class IdNome : IIdNome
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public override string ToString()
+        {
+            return Nome;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as IdNome);
+        }
+
+        public virtual bool Equals(IdNome obj)
+        {
+            return obj != null && string.Equals(obj.Id, Id);
+        }
+        public override int GetHashCode()
+        {
+            return Id != 0 ? Id.GetHashCode() : base.GetHashCode();
+        }
+    }
+}
