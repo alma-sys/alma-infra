7f3d67c97950f25c3d0ab09728125c411d30148a
Alex Durães <alexbduraes@gmail.com>
2017-01-28 15:33:06 -0200

    Projeto base para projetos de teste.

---
+++ Alma.Testes/DataBuilder/DataBuilderBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alma.Testes.DataBuilder
+{
+    public class DataBuilderBase
+    {
+        private static object lockGerarTag = new object();
+        public static string GerarTag()
+        {
+            lock (lockGerarTag)
+            {
+                Thread.Sleep(5);
+                var tag = "Z-" + DateTime.Now.ToString("yyMMddHHmmssfff");
+                return tag;
+            }
+        }
+    }
+}
