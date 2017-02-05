6c493f1e5a19e07ea4328836d9666e4cd88c47f7
Marcos Junior <junalmeida@gmail.com>
2017-02-05 12:33:23 -0200

    Mensagem padrão

--- Alma.Core/Validacao/ValidacaoException.cs
+++ Alma.Core/Validacao/ValidacaoException.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Alma.Core
 {
-    public class ValidacaoException : ApplicationException
+    public abstract class ValidacaoException : ApplicationException
     {
         public ValidacaoException(string message = null, IDictionary<string, string> errors = null)
-            : base(message)
+            : base(GetDefaultMessage(message, errors))
         {
             this.Errors = new ReadOnlyDictionary<string, string>(errors ?? new Dictionary<string, string>());
+        }
+
+        private static string GetDefaultMessage(string message, IDictionary<string, string> errors)
+        {
+            if (!string.IsNullOrWhiteSpace(message))
+                return message;
+            else if (errors != null && errors.Count > 0)
+                return errors.Values.First();
+            else
+                return null;
         }
 
         public IDictionary<string, string> Errors { get; private set; }
---
+++ Alma.Dominio/Validadores/ValidacaoException.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Alma.Dominio.Validadores
+{
+    internal class ValidacaoException : Alma.Core.ValidacaoException
+    {
+        public ValidacaoException(string message = null, IDictionary<string, string> errors = null)
+            : base(message, errors)
+        { }
+    }
+}
