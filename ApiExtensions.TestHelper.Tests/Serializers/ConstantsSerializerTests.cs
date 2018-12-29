0e0da77d79b2a61fa850155c11897cf4cda9a909
Marcos Junior <junalmeida@gmail.com>
2018-12-29 09:29:08 -0200

    Fix test run

--- ApiExtensions.TestHelper.Tests/Serializers/ConstantsSerializerTests.cs
+++ ApiExtensions.TestHelper.Tests/Serializers/ConstantsSerializerTests.cs
@@ -1,9 +1,4 @@
-﻿using Alma.Common;
-using Alma.Common.Dto;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Xunit;
+﻿using Xunit;
 
 namespace Alma.ApiExtensions.Serializers.Tests
 {
@@ -12,7 +7,7 @@ namespace Alma.ApiExtensions.Serializers.Tests
         enum ValuesToTest
         {
             Value1 = 1,
-            [System.ComponentModel.AmbientValue("VALUE-2")]
+            [System.ComponentModel.AmbientValue("VALUE2")]
             Value2 = 2,
             [System.ComponentModel.Description("The Value Is Three")]
             Value3 = 3
@@ -44,30 +39,14 @@ namespace Alma.ApiExtensions.Serializers.Tests
         [Fact()]
         public void ShouldSerializeEnumValuesIntoCodeDescription()
         {
-
-            var list = new List<CodeDescription>();
-
-            foreach (var item in Enum.GetValues(typeof(ValuesToTest)))
-            {
-                list.Add(((Enum)item).ToCodeDescription());
-            }
-
-            list = list.OrderBy(t => t.Code.ToLower()).ToList();
-            foreach (var l in list)
-            {
-                Console.WriteLine(l.Code);
-            }
-
-
             var resultado = ConstantSerializer.SerializeEnumToCode(typeof(ValuesToTest), true, true);
-            System.Console.WriteLine(resultado);
             Assert.Equal(@"[
   {
     ""code"": ""Value1"",
     ""description"": ""Value1""
   },
   {
-    ""code"": ""VALUE-2"",
+    ""code"": ""VALUE2"",
     ""description"": ""Value2""
   },
   {
